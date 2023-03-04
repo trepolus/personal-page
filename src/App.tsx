6403d3445b1c5751b647a2cf9607ce04c357a090
Lucas Kummer <lucas.kummer@thoughtworks.com>
2023-03-04 15:45:13 +0100

    Revamp homepage to use AI images

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
+import AIMindLogo from './images/mindAi.png';
 import './App.css';
+import backgroundImage from './images/volumetricShapes.png';
 
 function App() {
     return (
-        <div className="App">
+        <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                <h2>#AITakeover</h2>
+                <img src={AIMindLogo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
+                    <code>Everything is generated</code>!
                 </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
+                <a className="App-link" href="https://www.midjourney.com/" target="_blank" rel="noopener noreferrer">
+                    Reimagine your Imagination
                 </a>
             </header>
         </div>
